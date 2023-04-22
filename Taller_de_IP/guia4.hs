640ca52a25e1c6c489aca5eadad8976a3cf30c46
Camila Gallo García <camilagallo1700@gmail.com>
2023-04-22 12:59:42 -0300

    Se agregó la función todosDigitosIguales

--- Taller_de_IP/guia4.hs
+++ Taller_de_IP/guia4.hs
@@ -34,4 +34,9 @@ sumaDigitos n   | n < 10 = n
                 | otherwise = mod n 10 + sumaDigitos (div nRestado 10) 
                     where nRestado = n - mod n 10
                     
+--7
+todosDigitosIguales :: Integer -> Bool
+todosDigitosIguales n | n < 10 = True
+                        | otherwise = mod n 10 == mod (div n 10) 10 && todosDigitosIguales(div n 10)
+
 
