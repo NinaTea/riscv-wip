1921a919a68376d343832613cadd28ee617b3a44
Camila Gallo García <camilagallo1700@gmail.com>
2023-04-22 09:30:46 -0300

    Prueba

---
+++ Taller_de_IP/guia4.hs
@@ -0,0 +1,28 @@
+--1 fibonacci
+
+fibonacci :: Integer -> Integer 
+fibonacci n | n == 0 = 0
+            | n == 1 = 1
+            | otherwise = fibonacci (n-1) + fibonacci (n-2)
+
+--2
+parteEntera :: Float -> Integer
+parteEntera n   | 0 <= n && n < 1 = 0 
+                | n == 1 = 1
+                | otherwise = 1 + parteEntera(n - 1)
+
+--3 
+esDivisible :: (Integer, Integer) -> Bool
+esDivisible (a, b) | abs a  -  abs b == 0 = True
+                | abs a  -  abs b < 0 = False
+                | otherwise = esDivisible (abs (a-b), abs b) 
+
+--4
+impar :: Integer -> Integer
+impar n = 2*n + 1
+
+sumaImpares :: Integer -> Integer
+sumaImpares n   | n == 1 = 1
+                | n > 0 = 2*n-1 + sumaImpares (n-1)
+
+    
