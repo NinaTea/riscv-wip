184f26744b1a938169de2d4fbcfdca138b952b04
Cam <63563305+NinaTea@users.noreply.github.com>
2023-05-01 11:13:05 -0300

    Update guia5.hs
    Agregue la funcion pertenece

--- Taller_de_IP/guia5.hs
+++ Taller_de_IP/guia5.hs
@@ -14,3 +14,10 @@ reverso :: [t] -> [t]
 reverso [] = []
 reverso (x:xs) = reverse (x:xs)
 --soy re trucha ya se
+
+--2
+--1
+pertenece :: Eq t => t -> [t] -> Bool
+pertenece _ [] = False
+pertenece t (x:xs) | t == x = True
+                    | otherwise = pertenece t xs  
