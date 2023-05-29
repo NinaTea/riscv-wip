f62c470ea6595c2a6a3ad420953db71e3e6478ed
Cam <63563305+NinaTea@users.noreply.github.com>
2023-05-29 18:50:27 -0300

    Update tallerip.py

--- Taller_de_IP/tallerip.py
+++ Taller_de_IP/tallerip.py
@@ -1,39 +1,6 @@
-#es es la suma de todos los elementos de s
-def suma_elementos(s:list)-> int:
-    suma:int=0
-    for i in s:
-        suma+=i
-    return suma
-
-""" 
-ordenados (in s:seq<Z>) : Bool {
-requiere: { True }
-asegura: { res = true ↔ (∀i : Z)(0 ≤ i < (|s| − 1) → s[i] < s[i + 1]}
-"""
-def ordenados(s:list)->bool:
-    res:bool= False
-    i:int=0
-    while (i < len(s) - 1) and s[i] < s[i+1]:
-        i= i+1
-    if i == len(s)-1:
-        res=True
-    return res
-
-""" 
-dada una cadena de texto (string), devolver verdadero si  ́esta es pal ́ındroma (se lee igual en ambos sentidos), falso en
-caso contrario.
-"""
-def es_palindroma(s:list) -> bool:
-    res:bool=False
-    i:int=0
-    while i < len(s)//2 and s[i]==s[-1*(i+1)]:
-        i=i+1
-    if i == len(s)//2:
-        res=True
-    return res
-
 #segunda parte
-""" 
+import random
+"""
 1. Implementar una funcion que dada una lista de n ́umeros, en las posiciones pares borra el valor original y coloca un
 cero. Esta funci ́on modifica el par ́ametro ingresado. Nota: La lista ser ́a un tipo inout.
 """
@@ -57,6 +24,13 @@ def idem_anterior_pares(s:list)-> list:
             l[i] = 0
         i=i+1
     return l
+
+def sacaVocales(s:str) -> str:
+    res:str = ""
+    for i in s:
+        if i.lower() not in "aeiou":
+            res = res + i
+    return res
 
 """ 
 problema reemplazaVocales (in s:seq<Char>) : seq<Char> {
@@ -89,3 +63,50 @@ def daVueltaStr(s:str)-> str:
         l = l + s[-1*(i+1)]
         i=i+1
     return l
+
+def nombreDeEstudiantes():
+    nombreEstudiante:str = ""
+    nombresEstudiantes:list = []
+    while nombreEstudiante.lower()  != "listo":
+        nombreEstudiante = input("Ingrese nombre del estudiante. Cuando no quiera agregar mas, escriba: Listo ")
+        if nombreEstudiante != "listo":
+            nombresEstudiantes.append(nombreEstudiante)
+    return nombresEstudiantes
+
+def historial_monedero():
+    monedero:int = 0
+    historial:list = []
+    accion:str = ""
+    while accion != "x":
+        accion = input("Ingrese la operacion que quiere realizar: C; cargar, D; descontar, X; finalizar").lower()
+        if accion == "c":
+            monto = int(input("Ingrese el monto a cargar: Solo enteros"))
+            historial.append((accion, monto))
+            monedero = monedero + monto
+            print("El saldo actual en su monedero es: ", monedero)
+        else:
+            monto = int(input("Ingrese el monto a descontar: Solo enteros"))
+            historial.append((accion, monto))
+            monedero = monedero - monto
+            print("El saldo actual en su monedero es: ", monedero)
+
+def siete_y_medio():
+    valor:int = 0
+    cartas:list = []
+    accion:str = ""
+    while accion != "plantarse":
+        accion = input("te plantas o seguis? ")
+        if accion == "sigo":
+            val_posibles:list = [0,1,2,3,4,5,6,7,8,9,10,11,12]
+            numero_random = random.choices(val_posibles, weights=[1,1,1,1,1,1,1,1,0,0,1,1,1], k=1)[0] #devuelve una lista
+            cartas.append(numero_random)
+            if numero_random == 10 or numero_random == 11 or numero_random == 12:
+                valor = valor + 0.5
+            else:
+                valor = valor + 1
+            if valor > 7.5:
+                print("perdiste. Estas cartas te salieron: ", cartas)
+                accion = "plantarse"
+
+def perteneceACadaUno(s:list, e:int)->list:
+    res:bool=False
